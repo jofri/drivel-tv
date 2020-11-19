e07dc49b17d3c38b8e2a069e617e702e97f4233e
Bowenf3 <bowoliverj@gmail.com>
2020-11-19 12:43:37 +0000

    Test for merge

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+//Test comment
 // Import dependencies
 const express = require('express');
 const app = express();
@@ -7,14 +7,13 @@ const io = require('socket.io')(http);
 const path = require('path');
 const router = require('./router');
 const mongoose = require('mongoose');
-const {startAllCron} = require('./cron/cron-startup');
+const { startAllCron } = require('./cron/cron-startup');
 
 // Call io module with io instance
 require('./socket/broadcast-socket')(io);
 
 // If app is in dev mode, replace process.env variables with variables in .env file
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-
 
 // Parse API requests as JSON
 app.use(express.json());
@@ -28,22 +27,22 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-
 // Connect to MongoDB and listen for new requests
-http.listen(process.env.PORT, async (req, res) => { // eslint-disable-line no-unused-vars
+http.listen(process.env.PORT, async (req, res) => {
+  // eslint-disable-line no-unused-vars
   try {
     await mongoose.connect(process.env.MONGO_DB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
-      useCreateIndex: true,
+      useCreateIndex: true
     });
     // Function that finds all broadcasts in DB and start their timers
     await startAllCron();
-    console.log(`Drivel server connected to DB - listening on port: ${process.env.PORT}`);
+    console.log(
+      `Drivel server connected to DB - listening on port: ${process.env.PORT}`
+    );
   } catch (error) {
-    console.log('Could not connect to database', error);  // eslint-disable-line no-console
+    console.log('Could not connect to database', error); // eslint-disable-line no-console
   }
 });
-
-
