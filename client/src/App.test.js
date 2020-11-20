ae9ad299285351b9d7f9e25daea5838f97e3c0ae
Matt Haddon <matthaddon5@gmail.com>
2020-11-20 16:06:40 +0100

    testing works

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+// import App from './App';
+function sum(a, b) {
+  return a + b;
+}
+test('renders learn react link', () => {
+  expect(2 + 2).toBe(4);
+
+  //   //   render(<App />);
+  //   //   const linkElement = screen.getByText(/learn react/i);
+  //   // expect(linkElement).toBeInTheDocument();
+});
--- models/Broadcast-model.js
+++ models/Broadcast-model.js
@@ -1,24 +1,26 @@
-
 // Require mongoose
 const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
 
 // Create new Broadcast schema
-const BroadcastModel = new Schema({
-  broadcastId: { type: String, required: true, unique: true},
-  title: { type: String, required: true },
-  description: String,
-  tags: String,
-  thumbnailUrl: { type: String, required: true },
-  owner: { type: String, required: true },
-  isReversed: { type: Boolean, required: true },
-  youtubePlaylists: { type: Array, required: true },
-  videoArray: { type: Array, required: true },
-  currentVideo: { type: String, required: true },
-  currentVideoLength: { type: String, required: true },
-  currentTime: { type: Number, required: true },
-  nextVideo: { type: String, required: true },
-  nextVideoLength: { type: String, required: true },
-}, { timestamps: true }); // Set automatic timestamp for every document
+const BroadcastModel = new Schema(
+  {
+    broadcastId: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
+    description: String,
+    tags: String,
+    thumbnailUrl: { type: String, required: true },
+    owner: { type: String, required: true },
+    isReversed: { type: Boolean, required: true },
+    youtubePlaylists: { type: Array, required: true },
+    videoArray: { type: Array, required: true },
+    currentVideo: { type: String, required: true },
+    currentVideoLength: { type: String, required: true },
+    currentTime: { type: Number, required: true },
+    nextVideo: { type: String, required: true },
+    nextVideoLength: { type: String, required: true },
+  },
+  { timestamps: true }
+); // Set automatic timestamp for every document
 
 module.exports = mongoose.model('Broadcast', BroadcastModel);
