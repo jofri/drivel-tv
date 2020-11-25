8239127eae22c1406424f45d5dbefe7b388f3e22
Bowenf3 <bowoliverj@gmail.com>
2020-11-25 13:13:55 +0000

    Testing: Added 404 test and updated test description in create

--- client/src/components/Broadcastform.test.js
+++ client/src/components/Broadcastform.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Broadcastform from './Broadcastform.jsx';
 
-test('Delete form should render correctly', () => {
+test('Create form should render correctly', () => {
   render(<Broadcastform />);
   const expected = screen.getByText(/Create a new broadcast/);
   expect(expected).toBeInTheDocument();
