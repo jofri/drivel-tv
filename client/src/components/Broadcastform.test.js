958f5dcb6a133a41c1d1a77fe9d924bced33ec12
Bowenf3 <bowoliverj@gmail.com>
2020-11-25 13:10:38 +0000

    Testing: Added more test conditions to create and delete forms tests

--- client/src/components/Broadcastform.test.js
+++ client/src/components/Broadcastform.test.js
@@ -6,4 +6,20 @@ test('Delete form should render correctly', () => {
   render(<Broadcastform />);
   const expected = screen.getByText(/Create a new broadcast/);
   expect(expected).toBeInTheDocument();
+  const expected2 = screen.getByText(/Broadcast name:/);
+  expect(expected2).toBeInTheDocument();
+  const expected3 = screen.getByText(/Description:/);
+  expect(expected3).toBeInTheDocument();
+  const expected4 = screen.getByText(/Tags:/);
+  expect(expected4).toBeInTheDocument();
+  const expected5 = screen.getByText(/Owner:/);
+  expect(expected5).toBeInTheDocument();
+  const expected6 = screen.getByText(/Playlists:/);
+  expect(expected6).toBeInTheDocument();
+  const expected7 = screen.getByText(
+    /Want to reverse the queue order of the playlists?/
+  );
+  expect(expected7).toBeInTheDocument();
+  const expected8 = screen.getByText(/Create Broadcast/);
+  expect(expected8).toBeInTheDocument();
 });
--- client/src/components/Deletform.test.js
+++ client/src/components/Deletform.test.js
@@ -6,4 +6,8 @@ test('Delete form should render correctly', () => {
   render(<Deleteform />);
   const expected = screen.getByText(/Delete broadcast/);
   expect(expected).toBeInTheDocument();
+  const expected2 = screen.getByText(/Broadcast id:/);
+  expect(expected2).toBeInTheDocument();
+  const expected3 = screen.getByText(/Delete Broadcast/);
+  expect(expected3).toBeInTheDocument();
 });
